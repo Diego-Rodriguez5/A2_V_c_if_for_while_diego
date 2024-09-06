1d44b7f122b202355d077b2b932c51a8dea9d0b1
Diego-Rodriguez5 <a.22308051281296@cbtis128.edu.mx>
2024-09-06 09:33:10 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,26 @@
+'''Diego Rodriguez
+Trabajando con cadenas.
+'''
+
+nombre="Diego"
+print(nombre[0])
+print(nombre) 
+
+MisNovias=["Agripina","Anastasia","Maria"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("Otro ejemplo")
+    b = "Diego Rodriguez"
+print(b[2:5])
+print(" a mayusculas")
+a = "Diego Rodriguez"
+print(a.upper())
+print("Concatenar")
+
+#Para concatenar o combinar dos cadenas puedes utilizar el operador +.
+
+a = "Maria"
+b = "Perez"
+c = a + " "+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,21 @@
+import math
+edad=16 # 16 se guarda en edad
+
+nombre="Diego Rodriguez"
+print("Tu edad es:", edad)
+print("Tu nombre es:", nombre)
+print("-- tipo de variables --")
+
+x = str(7)    # x will be '3'
+y = int(3.1415)    # y will be 3
+Z = float(7)
+print(x)
+print(y)
+print(Z)
+print("-- Funciones m   atematicas --")
+x = math.sqrt(64)
+
+print(x)
+
+
+
